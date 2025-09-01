f0b01fb3b1482d1510a60386744ba244da4896f5
Prakash kumar <pengineer384@gmail.com>
2025-09-01 20:54:59 +0530

    Update provider.tf

--- Env/Dev/provider.tf
+++ Env/Dev/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.41.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "rg_dev"
+      storage_account_name = "storage0101"
+      container_name       = "pk-container"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
